ab6b98e9d7c2a9046ce72ac47913440409853fb5
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-22 17:19:09 +0800

    Update logs

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const start = async () => {
 
     app.listen(PORT, (err) => {
         if(err) console.log(`Error starting app: ${err}`)
-        else console.log(`Started listening on port ${PORT}`)
+        else console.log(`Tag Service started listening on port ${PORT}`)
     })
 }
 
